f632657c6dcb2cbbcc5a9d65304f4d8435f6cacb
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-12-09 11:46:10 -0500

    unpin jinja2 installed with conda-build

--- travis/install-conda-build.sh
+++ travis/install-conda-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get install -y -qq --no-install-recommends gcc
-conda install -q -y -c broad-viral -c r -c bioconda -c conda-forge -c defaults conda-build==3.0.25 anaconda-client jinja2==2.8
+conda install -q -y conda-build==3.0.25 anaconda-client jinja2
